65bb92a07630dbf99f49d9f75e7dbc5da8827b00
Ajith-P <47376329+Ajith-P@users.noreply.github.com>
2019-02-06 14:26:42 +0530

    Update voewl.java

--- voewl.java
+++ voewl.java
@@ -3,7 +3,7 @@ public class Main
  
        public static void main(String[] args) {
 
-        char ch = 's';
+        char ch = 'z';
 
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
