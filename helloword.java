8613c173340a9ada5e6874f87e65d032a761ac51
Ajith-P <47376329+Ajith-P@users.noreply.github.com>
2019-03-08 10:19:14 +0530

    Update helloword.java

--- helloword.java
+++ helloword.java
@@ -2,9 +2,9 @@
     {
     	public static void main(String[] args)
     	{
-    		for(int i = 0; i < 10; i++)
+    		for(int i = 0; i < 2; i++)
     		{
-    			System.out.println("Hello World!");
+    			System.out.println("Hello!");
     		}
     	}
     }
